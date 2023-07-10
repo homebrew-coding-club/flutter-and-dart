58a3efd3ca5b812b2df9dc05fd68c04ea74496d8
bannakonz <bannakon10@gmail.com>
2023-07-10 16:29:52 +0700

    more widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
-
 import 'package:flutter/material.dart';
-import 'package:first_app/start-screen.dart';
+import 'package:first_app/start_screen.dart';
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 5, 22, 213),
+                Color.fromARGB(255, 203, 194, 178),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.topRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
-    )
+    ),
   );
 }
