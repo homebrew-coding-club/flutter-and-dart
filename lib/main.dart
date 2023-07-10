1d0aa0bd6164c32fa54cdbc7e330fba66f029d25
bannakonz <bannakon10@gmail.com>
2023-07-10 22:33:20 +0700

    add state full

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
+import 'package:first_app/quiz.dart';
 import 'package:flutter/material.dart';
-import 'package:first_app/start_screen.dart';
+
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 5, 22, 213),
-                Color.fromARGB(255, 203, 194, 178),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.topRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
