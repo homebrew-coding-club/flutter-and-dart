58c6d0103134bb83b5c5a314b9c359b8e167dba0
bannakonz <bannakon10@gmail.com>
2023-07-10 14:45:47 +0700

    create widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
 
+import 'package:flutter/material.dart';
+import 'package:first_app/start-screen.dart';
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    )
+  );
+}
