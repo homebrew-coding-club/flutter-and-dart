74994000918a76e4ddd2ac44723b654436e63630
bannakonz <bannakon10@gmail.com>
2023-07-13 23:40:29 +0700

    add button reuse

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -19,18 +20,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
         children: [
           const Text('The Question...'),
           const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('answer 1'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('answer 2'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('answer 3'),
-          ),
+          AnswerButton(answerText: 'answer 1', onTap: () {}),
+          AnswerButton(answerText: 'answer 2', onTap: () {}),
+          AnswerButton(answerText: 'answer 3', onTap: () {}),
         ],
       ),
     );
