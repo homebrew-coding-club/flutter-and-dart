ed103c6dd8efe156a09b61755a568425c9e4e833
bannakonz <bannakon10@gmail.com>
2023-07-18 17:33:11 +0700

    textAlign button

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton({
-    super.key,
-    required this.answerText,
-    required this.onTap
-  });
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onTap});
 
   final String answerText;
   final void Function() onTap;
@@ -25,7 +22,10 @@ class AnswerButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(40),
         ),
       ),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
