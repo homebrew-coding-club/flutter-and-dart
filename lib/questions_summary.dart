a94073f9ced2da8ed9c89f4801cf6a98a9ce8a51
bannakonz <bannakon10@gmail.com>
2023-07-19 15:28:20 +0700

    formating

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class QuestionsSummary extends StatelessWidget {
-
   const QuestionsSummary(this.summaryData, {super.key});
 
   final List<Map<String, Object>> summaryData;
@@ -9,19 +8,27 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: summaryData.map((data) {
-        return Row(children: [
-          Text(((data['question'] as int) + 1).toString()),
-          Column(children: [
-            Text(data['question'] as String),
-            const SizedBox(height: 5,),
-            Text(data['user_answer'] as String,),
-            Text(data['correct_answer'] as String),
-          ],)
-        ],);
-      },
+      children: summaryData.map(
+        (data) {
+          return Row(
+            children: [
+              Text(((data['question'] as int) + 1).toString()),
+              Column(
+                children: [
+                  Text(data['question'] as String),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    data['user_answer'] as String,
+                  ),
+                  Text(data['correct_answer'] as String),
+                ],
+              )
+            ],
+          );
+        },
       ).toList(),
     );
   }
-
 }
